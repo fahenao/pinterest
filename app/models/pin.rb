2e2e9d1228f444e2739585aa8176c016d02aee4f
Felipe Henao <fahenao@MBP-de-Felipe.cpe.cableonda.net>
2015-12-22 16:34:07 -0500

    Added pagination to pins index

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,9 +2,8 @@ class Pin < ActiveRecord::Base
 	belongs_to :user
 
 	has_attached_file :image, 
-										:styles => { 
-											:medium => '300x300', 
-											:thumb => '100x100' }, 
-											:default_url => "http://www.reconregen.com/wp-content/uploads/2015/04/icon_missing_photo_detail.png"
-	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+										:styles => { :medium => '300x300', :thumb => '100x100' }
+	validates_attachment :image, content_type: { content_type: /\Aimage\/.*\Z/ }
+	validates :description, presence: true
+	validates :image, presence: true
 end
