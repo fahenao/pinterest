95c56a2a8cf12bd31223efc6d7fea101d6269955
Felipe Henao <fahenao@MBP-de-Felipe.cpe.cableonda.net>
2015-12-21 22:18:33 -0500

    Implemented devise for Users

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,5 +78,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   #require for heroku
-  config.action_mailer.default_url_options = { host: => 'anv-pinterest.heroku.com' }
+  config.action_mailer.default_url_options = { host: 'anv-pinterest.heroku.com' }
 end
