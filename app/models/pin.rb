939ec4db7e687ec3165a8daca03b0bd6cc87c38a
Felipe Henao <fahenao@MBP-de-Felipe.cpe.cableonda.net>
2015-12-22 12:21:06 -0500

    Add image upload with paperclip

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,3 +1,6 @@
 class Pin < ActiveRecord::Base
 	belongs_to :user
+
+	has_attached_file :image, :styles => { :medium => '300x300', :thumb => '100x100' }, default_url: "http://www.reconregen.com/wp-content/uploads/2015/04/icon_missing_photo_detail.png"
+	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 end
